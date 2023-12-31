a5676393cb75517b915ce23b872cf4fb52fc993f
VITLAP80 <VITLAP80@example.com>
2023-12-31 14:41:11 +0530

    Initial Commit

---
+++ src/javaCodingPractice/Static1.java
@@ -0,0 +1,13 @@
+package javaCodingPractice;
+
+public class Static1 {
+	private static void SayMyName() {
+		System.out.println("My Name is Ravindran");
+
+	}
+	public static void main(String[] args) {
+
+		SayMyName();
+	}
+
+}
